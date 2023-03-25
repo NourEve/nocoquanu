13be14e09b4e0e5e01d9bf5340c671f2748c9369
Corentin Miserque <corentinmiserque1@gmail.com>
2023-03-25 19:39:27 +0100

    modif

--- src/React/Loginpage/Loginpage.jsx
+++ src/React/Loginpage/Loginpage.jsx
@@ -1,51 +1,66 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import logo from "../../../public/assets/nocoquanu-logo.png";
+import scout from "../../../public/assets/scout.gif";
 
-function Loginpage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Loginpage = () => {
+  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Faire quelque chose avec les informations d'identification ici
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios({
+      method: "get",
+      url: `https://test.hugoorickx.tech/con/${username}/${password}`,
+      responseType: "json",
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <main>
-        <div className="loginpage">
-
-            <div className="logo_container">
-            <img className="logo"src={logo} alt="logo" />
-            </div>
-
-            <div className="form">
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">Nom d'utilisateur :</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-
-                    <label htmlFor="password">Mot de passe :</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-
-                    <button type="submit">Login</button>
-                </form>
-            </div>
+      <div className="loginpage">
+        <div className="logo_container">
+          <img className="logo" src={logo} alt="logo" />
         </div>
+
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Nom d'utilisateur :</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+
+            <label htmlFor="password">Mot de passe :</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            <button type="submit">Connexion</button>
+          </form>
+        </div>
+        <img className="scout" src={scout} alt="scout" />
+      </div>
     </main>
   );
-}
+};
 
 export default Loginpage;
+
