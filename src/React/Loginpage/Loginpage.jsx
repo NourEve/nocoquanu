bd9888fcf2434dda45da79dcd387e92fbf4913cc
Nour Everaert <nour.everaert.prof@gmail.com>
2023-03-25 17:42:53 +0100

    ajout images

--- src/React/Loginpage/Loginpage.jsx
+++ src/React/Loginpage/Loginpage.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import logo from "../../../public/assets/nocoquanu-logo.png";
+import { useState } from "react";
+import logo from "../../../public/assets/nocoquanu-logo.webp";
 import gif from "../../../public/assets//scout.gif";
 
 function Loginpage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
@@ -15,45 +15,41 @@ function Loginpage() {
 
   return (
     <main>
-        <div className="loginpage">
-
-            <div className="logo_container">
-            <img className="logo"src={logo} alt="logo" />
-            </div>
-
-            <div className="form">
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">Nom d'utilisateur :</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-
-                    <label htmlFor="password">Mot de passe :</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-
-                    <button type="submit">Connexion</button>
-                    
-                </form>
-            </div>
-
-       
-              <div className="gif">
-                <img src={gif} alt="gif" />
-              </div>
-            
+      <div className="loginpage">
+        <div className="logo_container">
+          <img className="logo" src={logo} alt="logo" />
         </div>
+
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Nom d'utilisateur :</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+
+            <label htmlFor="password">Mot de passe :</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+
+            <button type="submit">Connexion</button>
+          </form>
+        </div>
+
+        <div className="gif">
+          <img src={gif} alt="gif" />
+        </div>
+      </div>
     </main>
   );
 }
--- src/React/MapPage/MapPage.jsx
+++ src/React/MapPage/MapPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MapPage = () => {
   return (
     <div>
-      <p>Test map</p>
+      <Link to={"/equipe"}>
+        <h2>Equipe 1</h2>
+      </Link>
+      <Link to={"/equipe"}>
+        <h2>Equipe 2</h2>
+      </Link>
+      <Link to={"/equipe"}>
+        <h2>Equipe 3</h2>
+      </Link>
+      <Link to={"/equipe"}>
+        <h2>Equipe 4</h2>
+      </Link>
     </div>
   );
 };
